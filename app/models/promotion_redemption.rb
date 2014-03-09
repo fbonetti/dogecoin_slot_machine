3be71b2947409d903db910359096cd7d989b44c8
frankbonetti <frankbonetti@frankbonetti-laptop.(none)>
2014-03-08 18:15:03 -0600

    update user approximate balance after promotion_redemption creation

--- app/models/promotion_redemption.rb
+++ app/models/promotion_redemption.rb
@@ -8,6 +8,8 @@ class PromotionRedemption < ActiveRecord::Base
   validates_uniqueness_of :ip_address, scope: :promotion_id, message: "cannot redeem a promo code more than once"
   validate :must_not_exceed_promotion_limit
 
+  after_create :update_approximate_user_balance
+
   def self.total_amount
     PromotionRedemption.sum(:amount)
   end
@@ -19,4 +21,9 @@ class PromotionRedemption < ActiveRecord::Base
       errors.add(:code, "has expired")
     end
   end
+
+  def update_approximate_user_balance
+    self.user.update_approximate_balance
+  end
+
 end
